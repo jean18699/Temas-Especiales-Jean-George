2e9f563e1f25029a522f9e6b7f6e9205941d88f4
jean <jean18699@gmail.com>
2021-09-16 19:19:43 -0400

    implementando radio groups

--- Practica1/app/src/main/java/com/pucmm/practica1/MainActivity.java
+++ Practica1/app/src/main/java/com/pucmm/practica1/MainActivity.java
@@ -2,8 +2,13 @@ package com.pucmm.practica1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 import java.util.ArrayList;
@@ -16,13 +21,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Puente entre actividades
+        Intent summary = new Intent(MainActivity.this,SummaryActivity.class);
+
+
+
+        Button btnEnviar = findViewById(R.id.btnEnviar);
+        EditText editNombre = findViewById(R.id.editNombre);
+        EditText editApellido = findViewById(R.id.editApellido);
         Spinner spnGenero =  findViewById(R.id.spnGenero);
+
+
         List<String> generos = new ArrayList<>();
         generos.add("Masculino");
         generos.add("Femenino");
 
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this,android.R.layout.simple_spinner_dropdown_item,generos);
         spnGenero.setAdapter(adapter);
+
+
+        btnEnviar.setOnClickListener(v -> {
+
+            summary.putExtra("nombre",editNombre.getText().toString());
+            summary.putExtra("apellido",editApellido.getText().toString());
+            summary.putExtra("genero",spnGenero.getSelectedItem().toString());
+            startActivity(summary);
+        });
+
 
 
     }
--- Practica1/app/src/main/java/com/pucmm/practica1/SummaryActivity.java
+++ Practica1/app/src/main/java/com/pucmm/practica1/SummaryActivity.java
@@ -3,6 +3,8 @@ package com.pucmm.practica1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 public class SummaryActivity extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class SummaryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_summary);
+
+        Bundle extras = getIntent().getExtras();
+        TextView txtFraseNombre = findViewById(R.id.txtFraseNombre);
+
+
+        if(extras != null){
+            txtFraseNombre.setText("Hola!, Mi nombre es: "+extras.getString("nombre"));
+
+        }
+
+
     }
 }
