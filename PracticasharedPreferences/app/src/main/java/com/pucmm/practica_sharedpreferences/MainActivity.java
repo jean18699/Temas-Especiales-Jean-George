6c2f8d096d0a6d6062ca32d1d870fa0aec0a310f
jean <jean18699@gmail.com>
2021-10-27 23:25:40 -0400

    creacion de proyecto y de fragmento de lista

---
+++ PracticasharedPreferences/app/src/main/java/com/pucmm/practica_sharedpreferences/MainActivity.java
@@ -0,0 +1,14 @@
+package com.pucmm.practica_sharedpreferences;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
