167c9c0907cf7d06c4f98d3386852de962f6bd53
jean <jean18699@gmail.com>
2021-11-09 02:25:13 -0400

    Creacion de proyecto

---
+++ ProyectoFinal/app/src/main/java/com/pucmm/proyectofinal/roomviewmodel/activities/MainRoomActivity.java
@@ -0,0 +1,22 @@
+package com.pucmm.proyectofinal.roomviewmodel.activities;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import androidx.recyclerview.widget.RecyclerView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.pucmm.proyectofinal.R;
+
+
+public class MainRoomActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+
+    }
+}
